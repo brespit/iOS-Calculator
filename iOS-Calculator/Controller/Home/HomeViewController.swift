abc96b924608ad81ada1f899db83bd519d7fc769
Flavio Leite <flaviorcleite@gmail.com>
2019-07-24 13:23:47 +0200

    Outlets, Actions y Button Extension

--- iOS-Calculator/Controller/Home/HomeViewController.swift
+++ iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 final class HomeViewController: UIViewController {
 
+    // MARKL: Outlets
+    
+    //Result
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    //Numbers
+    @IBOutlet weak var number0: UIButton!
+    @IBOutlet weak var number1: UIButton!
+    @IBOutlet weak var number2: UIButton!
+    @IBOutlet weak var number3: UIButton!
+    @IBOutlet weak var number4: UIButton!
+    @IBOutlet weak var number5: UIButton!
+    @IBOutlet weak var number6: UIButton!
+    @IBOutlet weak var number7: UIButton!
+    @IBOutlet weak var number8: UIButton!
+    @IBOutlet weak var number9: UIButton!
+    @IBOutlet weak var numberDecimal: UIButton!
+    
+    //Operator
+    @IBOutlet weak var operatorAC: UIButton!
+    @IBOutlet weak var operatorPlusMinus: UIButton!
+    @IBOutlet weak var operatorPercent: UIButton!
+    @IBOutlet weak var operatorResult: UIButton!
+    @IBOutlet weak var opertatorAdiction: UIButton!
+    @IBOutlet weak var operatorSubstraction: UIButton!
+    @IBOutlet weak var operatorMultiplication: UIButton!
+    @IBOutlet weak var operatorDivision: UIButton!
+    
+    
+    
     // MARK: Initialization
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -25,8 +55,68 @@ final class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        number0.round()
+        number1.round()
+        number2.round()
+        number3.round()
+        number4.round()
+        number5.round()
+        number6.round()
+        number7.round()
+        number8.round()
+        number9.round()
+        numberDecimal.round()
+        operatorAC.round()
+        operatorResult.round()
+        operatorPercent.round()
+        operatorDivision.round()
+        operatorPlusMinus.round()
+        opertatorAdiction.round()
+        operatorMultiplication.round()
+        operatorSubstraction.round()
     }
 
-
+    // MARK: Button Actions
+    @IBAction func operatorACAction(_ sender: UIButton) {
+        sender.shine()
+    }
+    
+    @IBAction func operatorPlusMinusAction(_ sender: UIButton) {
+        sender.shine()
+    }
+    
+    @IBAction func operatorPercentAction(_ sender: UIButton) {
+        sender.shine()
+    }
+    
+    @IBAction func operatorResultAction(_ sender: UIButton) {
+        sender.shine()
+    }
+    
+    @IBAction func opertatorAdictionAction(_ sender: UIButton) {
+        sender.shine()
+    }
+    
+    @IBAction func operatorSubstractionAction(_ sender: UIButton) {
+        sender.shine()
+    }
+    
+    @IBAction func operatorMultiplicationAction(_ sender: UIButton) {
+        sender.shine()
+    }
+    
+    @IBAction func operatorDivisionAction(_ sender: UIButton) {
+        sender.shine()
+    }
+    
+    @IBAction func numberDecimalAction(_ sender: UIButton) {
+        sender.shine()
+    }
+    
+    @IBAction func numberAction(_ sender: UIButton) {
+        sender.shine()
+        print(sender.tag)
+    }
+    
 }
+
---
+++ iOS-Calculator/Util/UIButtonExtension.swift
@@ -0,0 +1,26 @@
+//
+//  UIButtonExtension.swift
+//  iOS-Calculator
+//
+//  Created by Flavio Leite on 24/07/2019.
+//  Copyright © 2019 Flavio Leite. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    func round() {
+        layer.cornerRadius = bounds.height/2 - 5 
+        clipsToBounds = true
+    }
+    
+    func shine() {
+        UIView.animate(withDuration: 0.1, animations: {
+            self.alpha = 0.5
+        }) { (completion) in
+            UIView.animate(withDuration: 0.1, animations: { self.transform = .identity })
+        }
+    }
+
+}
