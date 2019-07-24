d3d127f0cbcd459c165ccddada32e041b00abf96
Flavio Leite <flaviorcleite@gmail.com>
2019-07-24 12:09:07 +0200

    Instanciar la pantalla inicial

--- iOS-Calculator/AppDelegate.swift
+++ iOS-Calculator/AppDelegate.swift
@@ -16,7 +16,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+        setupView()
+        
         return true
+    }
+    
+    // MARK: private methods
+    private func setupView() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        
     }
 
 }
---
+++ iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.swift
+//  iOS-Calculator
+//
+//  Created by Flavio Leite on 24/07/2019.
+//  Copyright © 2019 Flavio Leite. All rights reserved.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    // MARK: Initialization
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: Life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
